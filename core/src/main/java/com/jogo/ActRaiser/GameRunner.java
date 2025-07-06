4e7d42240ca67d797c049eb5aaec39dad1c4367c
alcPresunto <guilhermeasouza906@gmail.com>
2025-07-06 00:37:55 -0300

    Configurações da inicialização

--- core/src/main/java/com/jogo/ActRaiser/GameRunner.java
+++ core/src/main/java/com/jogo/ActRaiser/GameRunner.java
@@ -5,9 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class GameRunner extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
@@ -20,7 +18,8 @@ public class GameRunner extends ApplicationAdapter {
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         batch.draw(image, 140, 210);
         batch.end();
