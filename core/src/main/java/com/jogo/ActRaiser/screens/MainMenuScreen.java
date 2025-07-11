72406382870541b66b890f816462c6808d2d6bca
alcPresunto <guilhermeasouza906@gmail.com>
2025-07-10 21:28:10 -0300

    Menu simples

--- core/src/main/java/com/jogo/ActRaiser/screens/MainMenuScreen.java
+++ core/src/main/java/com/jogo/ActRaiser/screens/MainMenuScreen.java
@@ -3,13 +3,24 @@ package com.jogo.ActRaiser.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.jogo.ActRaiser.GameRunner;
 
 public class MainMenuScreen implements Screen {
     private final GameRunner gameRunner;
+    private Texture botaoJogar;
+
+    private float botaoX, botaoY, botaoWidth, botaoHeight;
 
     public MainMenuScreen(GameRunner gameRunner) {
         this.gameRunner = gameRunner;
+        botaoJogar = new Texture(Gdx.files.internal("assets/botaoJogar.png"));
+        gameRunner.font.getData().setScale(1.5f);
+
+        botaoWidth = botaoJogar.getWidth();
+        botaoHeight = botaoJogar.getHeight();
+        botaoX = (Gdx.graphics.getWidth() - botaoWidth) / 2;
+        botaoY = (Gdx.graphics.getHeight() - botaoHeight) / 2;
     }
 
     @Override
@@ -18,9 +29,21 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 1, 1);
+        Gdx.gl.glClearColor(0.3f, 0.3f, 0.3f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        float mouseX = Gdx.input.getX();
+        float mouseY = Gdx.graphics.getHeight() - Gdx.input.getY();
+
+        boolean isHovering = mouseX >= botaoX && mouseX <= botaoX + botaoWidth && mouseY >= botaoY
+                && mouseY <= botaoY + botaoHeight;
+
+        if (Gdx.input.justTouched() && isHovering) {
+            gameRunner.setScreen(new MainGameScreen(gameRunner));
+        }
+
         gameRunner.batch.begin();
+        gameRunner.batch.draw(botaoJogar, botaoX, botaoY);
         gameRunner.batch.end();
     }
 
@@ -42,6 +65,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void dispose() {
+        botaoJogar.dispose();
     }
 
 }
